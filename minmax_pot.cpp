42d3366140b2b764185ecc9875938ff3bed88145
lambozsolty <56071289+lambozsolty@users.noreply.github.com>
2020-06-01 15:05:57 +0300

    Add files via upload

--- minmax_pot.cpp
+++ minmax_pot.cpp
@@ -7,15 +7,36 @@ using namespace std;
 struct Node
 {
     int value;
-    vector<Node*> childs;
+    vector<Node*> children;
 };
 
+int alfabeta(Node*, int, int, int, bool);
 int max(int, int);
 int min(int, int);
+Node* pushNode(int);
 
 int main()
 {
+    Node* root = pushNode(0);
+    root->children.push_back(pushNode(0));
+    root->children.push_back(pushNode(0));
+    root->children.push_back(pushNode(0));
+    root->children.push_back(pushNode(0));
 
+    root->children[0]->children.push_back(pushNode(4));
+    root->children[0]->children.push_back(pushNode(10));
+
+    root->children[1]->children.push_back(pushNode(3));
+    root->children[1]->children.push_back(pushNode(7));
+    root->children[1]->children.push_back(pushNode(8));
+
+    root->children[2]->children.push_back(pushNode(2));
+    root->children[2]->children.push_back(pushNode(6));
+    root->children[2]->children.push_back(pushNode(9));
+
+    root->children[3]->children.push_back(pushNode(1));
+
+    cout << alfabeta(root, 2, INT_MIN, INT_MAX, true);
 
     return 0;
 }
@@ -24,7 +45,7 @@ int alfabeta(Node* node, int melyseg, int alfa, int beta, bool is_maximizing)
 {
     int ertek;
 
-    if (melyseg == 0 || node->childs.size() == 0)
+    if (melyseg == 0 || node->children.size() == 0)
     {
         return node->value;
     }
@@ -33,9 +54,9 @@ int alfabeta(Node* node, int melyseg, int alfa, int beta, bool is_maximizing)
     {
         ertek = INT_MIN;
 
-        for (int i = 0; i < node->childs.size(); i++)
+        for (int i = 0; i < node->children.size(); i++)
         {
-            ertek = max(ertek, alfabeta(node->childs[i], melyseg - 1, alfa, beta, false));
+            ertek = max(ertek, alfabeta(node->children[i], melyseg - 1, alfa, beta, false));
 
             alfa = max(alfa, ertek);
 
@@ -51,9 +72,9 @@ int alfabeta(Node* node, int melyseg, int alfa, int beta, bool is_maximizing)
     {
         ertek = INT_MAX;
 
-        for (int i = 0; i < node->childs.size(); i++)
+        for (int i = 0; i < node->children.size(); i++)
         {
-            ertek = min(ertek, alfabeta(node->childs[i], melyseg - 1, alfa, beta, true));
+            ertek = min(ertek, alfabeta(node->children[i], melyseg - 1, alfa, beta, true));
 
             beta = min(beta, ertek);
 
@@ -75,5 +96,13 @@ int max(int a, int b)
 int min(int a, int b)
 {
     return a < b ? a : b;
+}
+
+Node* pushNode(int value)
+{
+    Node* newnode = new Node;
+    newnode->value = value;
+
+    return newnode;
 }
 
